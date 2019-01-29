a2630f85b13e1ab6870dc33d1042ec7ff29d8216
liveinmegacity <33742639+liveinmegacity@users.noreply.github.com>
2019-01-29 12:45:21 +0700

    Add balanced brackets algorithm

--- BalancedBrackets/Program.cs
+++ BalancedBrackets/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BalancedBrackets
 {
     class Program
     {
+        private struct BalanceCase
+        {
+            public string Expression { get; set; }
+            public bool IsBalanced { get; set; }
+        }
+
+        private static readonly List<BalanceCase> BalanceCases = new List<BalanceCase>() {
+            new BalanceCase() { Expression = "((1+3)()(4+(3-5)))", IsBalanced = true },
+            new BalanceCase() { Expression = "", IsBalanced = true },
+            new BalanceCase() { Expression = "()", IsBalanced = true },
+            new BalanceCase() { Expression = "(i)", IsBalanced = true },
+            new BalanceCase() { Expression = "(l))", IsBalanced = false },
+            new BalanceCase() { Expression = "((a)", IsBalanced = false },
+            new BalanceCase() { Expression = ")", IsBalanced = false },
+            new BalanceCase() { Expression = "(", IsBalanced = false }
+        };
+
+
+
         static bool IsBalanced(string text, char open = '(', char close = ')')
         {
             var level = 0;
@@ -28,9 +45,15 @@ namespace BalancedBrackets
             }
             return level == 0;
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(IsBalanced("((1+3)()(4+(3-5)))"));
+            Console.WriteLine("\n  BalancedBrackets Test\n-------------------------\n");
+            foreach (var item in BalanceCases)
+            {
+                Assert.AreEqual(item.IsBalanced, IsBalanced(item.Expression));
+                Console.WriteLine("  {0,18}  {1,8}\n", item.Expression, item.IsBalanced);
+            }
         }
     }
 }
